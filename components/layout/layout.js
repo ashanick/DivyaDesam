b8c76524a0139a05a958ec1f1f26712961e09c4e
ashanick <asha.sundararajan@gmail.com>
2022-05-27 10:32:46 -0700

    test1

---
+++ components/layout/layout.js
@@ -0,0 +1,9 @@
+function Layout(props) {
+    return (
+        <div>
+            Hello Layout
+        </div>
+    )
+}
+
+export default Layout
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
 import '../styles/globals.css'
 import '../styles/design-tokens.css'
 import '../styles/utilities.css'
+import Layout from '../components/layout/layout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Head>
+        <title>Genius</title>
+        <meta name='description' content='NextJS Events' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
+      <Component {...pageProps} />)
+    </Layout>
+  )
 }
 
 export default MyApp
