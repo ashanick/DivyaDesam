35ff2c8186633632103356bca20c2eae57f9a28b
ashanick <asha.sundararajan@gmail.com>
2022-05-27 12:38:31 -0700

    Test4 Nav

--- components/layout/main-header.js
+++ components/layout/main-header.js
@@ -1,10 +1,43 @@
+import Link from "next/link";
+import Image from "next/image";
+
 import classes from './main-header.module.css'
 
 function MainHeader(){
     return (
         <header className={classes.header}>
-            <div>
-                <h1>Header</h1>
+            <div className={classes.headermenu}>
+                <Link href="/">
+                    <a>
+                        <div className={classes.image}>
+                            <Image  
+                                src={'/images/Iyengars logo.png'}
+                                alt="1000+ Iyengars"
+                                width={200}
+                                height={150}
+                                layout='responsive'
+                            />
+                            {/* Funny For image to show, we need this para */}
+                           <p style={{color: 'white'}}>Compendium of Iyengars
+                            </p>                         
+                        </div>
+                    </a>
+                </Link>
+
+                <nav className={classes.navigation}>
+                    <ul className={classes.links}>
+                        <li>
+                            <Link href="/clans">Clans</Link>
+                        </li>
+                        <li>
+                            <Link href="/search-links">Links</Link>
+                        </li>
+
+                        <li> 
+                            <Link href="/contactus">Contact</Link>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </header>
     )
