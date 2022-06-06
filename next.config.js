950b55c1032ed87274f443d7541b32b12f549f17
ashanick <asha.sundararajan@gmail.com>
2022-06-06 16:39:42 -0700

    June 6: test 1

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
+// module.exports = {
   env: {
     neo4j_username: 'neo4j',
     neo4j_password: 'kXQvU8aeNdreD6LeEdSPtA29TlwHz2LgvN2OoM2brl0'
   }
 }
 
+module.exports = nextConfig
